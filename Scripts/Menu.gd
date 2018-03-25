06c9eff45f0b9d02116fdb8bb735669eab5952ce
victo <victo_10_@hotmail.com>
2018-03-24 21:21:47 -0300

    Networked lobby

---
+++ Scripts/Menu.gd
@@ -0,0 +1,63 @@
+extends Control
+
+onready var name_line_edit = get_node("Panel/Main/NameLineEdit")
+onready var settings_panel = get_node("Panel/Main/SettingsPanel")
+onready var main_panel = get_node("Panel/Main")
+onready var lobby_panel = get_node("Panel/Lobby")
+onready var lobby_player_list = get_node("Panel/Lobby/LobbyPlayersList")
+onready var error_popup_name_empty = get_node("ErrorPopup")
+
+func _ready():
+	NetworkManager.connect("connection_succeeded", self, "connection_succeeded")
+	NetworkManager.connect("player_list_changed", self, "update_lobby")
+	pass
+
+func update_lobby():
+	lobby_player_list.clear()
+	for p in NetworkManager.players:
+		lobby_player_list.add_item(p.name)
+	pass
+
+func connection_succeeded():
+	main_panel.hide()
+	lobby_panel.show()
+	if NetworkManager.is_host():
+		lobby_panel.get_node("StartGameButton").show()
+	else:
+		lobby_panel.get_node("WaitingPlayersLabel").show()
+	pass
+
+func check_empty_name():
+	if name_line_edit.text != "":		
+		return true
+	else:
+		error_popup_name_empty.popup_centered()
+		return false
+	pass
+
+func _on_SettingsButton_pressed():
+	if settings_panel.is_visible_in_tree():
+		settings_panel.hide()
+	else:
+		settings_panel.show()
+		get_node("Panel/Main/SettingsPanel/ServerIpLineEdit").text = NetworkManager.SERVER_IP
+	pass
+
+func _on_SetServerIPButton_pressed():
+	NetworkManager.SERVER_IP = get_node("MainPanel/SettingsPanel/ServerIpLineEdit").text
+	pass 
+
+func _on_ConnectButton_pressed():
+	if check_empty_name():
+		NetworkManager.connect_player(name_line_edit.text)
+	pass
+
+func _on_HostButton_pressed():
+	if check_empty_name():
+		NetworkManager.host_server(name_line_edit.text)
+	pass
+
+
+func _on_OkButton_pressed():
+	error_popup_name_empty.hide()
+	pass
--- Scripts/NetworkManager.gd
+++ Scripts/NetworkManager.gd
@@ -1,15 +1,63 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+const PORT = 3333
+const SERVER_MAX_PLAYERS = 16
+var SERVER_IP = "127.0.0.1"
+
+var player_name
+var players = []
+
+signal player_list_changed()
+signal connection_succeeded()
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
+	get_tree().connect("connected_to_server", self, "_connected_ok")
+	get_tree().connect("connection_failed", self, "_connected_fail")
+	get_tree().connect("server_disconnected", self, "_server_disconnected")
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _connected_ok():
+	rpc("register_player", get_tree().get_network_unique_id(), player_name)
+	register_player(get_tree().get_network_unique_id(), player_name)
+	emit_signal("connection_succeeded")
+	pass
+
+func is_host():
+	if get_tree().get_network_unique_id() == 1:
+		return true
+	else:
+		return false
+	pass
+
+remote func update_player_list(players_list):
+	players = players_list
+	emit_signal("player_list_changed")
+	pass
+
+remote func register_player(id, name):
+	var player = {}
+	player.id = id
+	player.name = name
+	if is_host():
+		if id != 1:
+			for p in players:
+				rpc_id(id, "register_player", p.id, p.name)
+		players.append(player)
+		emit_signal("player_list_changed")
+		rpc("update_player_list", players)
+	pass
+
+func host_server(player_info):
+	var host = NetworkedMultiplayerENet.new()
+	host.create_server(PORT, SERVER_MAX_PLAYERS)
+	get_tree().set_network_peer(host)
+	player_name = player_info
+	_connected_ok()
+	pass
+
+func connect_player(player_info):
+	var host = NetworkedMultiplayerENet.new()
+	host.create_client(SERVER_IP, PORT)
+	get_tree().set_network_peer(host)
+	player_name = player_info
+	pass
