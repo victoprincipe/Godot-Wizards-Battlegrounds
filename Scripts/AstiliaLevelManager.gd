8a244fb86c5db4d10c57cfd2db0779ce6b6a5d80
Victor Principe Pessoa Borgues <victopessoa@imd.local>
2018-04-06 18:29:47 -0300

    Time to start the game in lobby

--- Scripts/AstiliaLevelManager.gd
+++ Scripts/AstiliaLevelManager.gd
@@ -1,7 +1,8 @@
 extends Node
 
-var time_to_start = 50
-var has_started = false
+onready var host_time_to_start = 3
+slave var time_to_start = 0
+slave var has_started = false
 var spawn_position = 0
 var lobby_positions = []
 
@@ -13,7 +14,20 @@ sync func level_loaded():
 		spawn_position += 1
 	pass
 
+func wait_for_players_load():
+	while not has_started:
+		if NetworkManager.is_host() and host_time_to_start >= 1:
+			host_time_to_start -= get_process_delta_time()			
+			rset("time_to_start", host_time_to_start)
+		if NetworkManager.is_host() and host_time_to_start <= 1:
+			has_started = true 
+			rpc("has_started", has_started)
+		yield(get_tree(), "idle_frame")
+	pass
+
 func _ready():
 	lobby_positions = get_node("Positions/LobbySpawnPositions").get_children()
 	rpc("level_loaded")
+	if NetworkManager.is_host():
+		wait_for_players_load()
 	pass
