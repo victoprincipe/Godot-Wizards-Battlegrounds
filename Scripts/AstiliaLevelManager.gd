cc6f9cb396eca5f5aa7f89b17f25d685eb91dd4b
Runewather <victo_10_@hotmail.com>
2018-05-20 02:12:02 -0300

    fixed spawn position bug

--- Scripts/AstiliaLevelManager.gd
+++ Scripts/AstiliaLevelManager.gd
@@ -5,19 +5,29 @@ slave var time_to_start = 0
 slave var has_started = false
 var spawn_position = 0
 var lobby_positions = []
+var loaded_players = []
 
 onready var game_manager = get_node("GameManager")
 
-sync func level_loaded(id):
-	if not has_started and spawn_position <= lobby_positions.size():
-		game_manager.spawn_player(id, lobby_positions[spawn_position].position)
-		spawn_position += 1
+remote func level_loaded(id):
+	loaded_players.append(id)
+	if loaded_players.size() == NetworkManager.players.size():
+		start_match()
+	pass
+
+func start_match():
+	for i in range(loaded_players.size()):
+		rpc("spawn_players", loaded_players[i], lobby_positions[i].global_position)
+	pass
+	
+sync func spawn_players(id, position):
+	game_manager.spawn_player(id, position)
 	pass
 
 func wait_for_players_load():
 	while not has_started:
 		if NetworkManager.is_host() and host_time_to_start >= 1:
-			host_time_to_start -= get_process_delta_time()			
+			host_time_to_start -= get_process_delta_time()
 			rset("time_to_start", host_time_to_start)
 		if NetworkManager.is_host() and host_time_to_start <= 1:
 			has_started = true 
@@ -27,7 +37,9 @@ func wait_for_players_load():
 
 func _ready():
 	lobby_positions = get_node("Positions/LobbySpawnPositions").get_children()
-	rpc("level_loaded", get_tree().get_network_unique_id())
 	if NetworkManager.is_host():
+		loaded_players.append(get_tree().get_network_unique_id())
 		wait_for_players_load()
+	if not NetworkManager.is_host():
+		rpc_id(1, "level_loaded", get_tree().get_network_unique_id())
 	pass
